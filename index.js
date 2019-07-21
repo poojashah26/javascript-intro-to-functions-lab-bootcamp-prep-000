c34d24dca206d7d287115a445f35a0ce01835289
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-07-21 11:06:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function shout(string) {
 }
 function whisper(string) {
   return string.toLowerCase()
+<<<<<<< HEAD
 }
 function logShout(string) {
   return console.log(string.toUpperCase())
@@ -14,4 +15,6 @@ function sayHiToGrandma(string) {
   if (string===string.toLowerCase()) return "I can't hear you!";
   else if (string===string.toUpperCase()) return "YES INDEED!";
   else if (string==="I love you, Grandma.") return "I love you, too."
+=======
+>>>>>>> 0f734453eb33a2df8e42b2419ae124be6bbfa52b
 }
